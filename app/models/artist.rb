351f9cb5d9ea1cf4ca511e17d941aae898713737
github username <github email address>
2019-07-22 17:13:33 -0700

    Lab complete

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,10 +1,47 @@
 class Artist
 
   attr_reader :name, :years_experience
+  @@all = []
 
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+  end
+
+  def self.all
+    @@all
+  end
+
+  def paintings
+    Painting.all.select do |painting|
+      painting.artist == self
+    end
+  end
+
+  def galleries
+    paintings.map do |painting|
+      painting.gallery
+    end
+  end
+
+  def cities
+    galleries.map do |gallery|
+      gallery.city
+    end
+  end
+
+  def self.total_experience
+    self.all.years_experience.sum
+  end
+
+  def self.most_prolific
+    self.all.max_by do |artist|
+      artist.paintings.length / artist.years_experience
+    end
+  end
+
+  def create_painting(title, price, gallery)
+    Painting.new(title, price, gallery, self)
   end
 
 end
